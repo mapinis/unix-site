934efb07028b17277448ba0dd0d7c5832966d707
Mark Apinis <mapinis2019@gmail.com>
2017-08-11 13:50:18 -0700

    help command

--- src/rec/commands.js
+++ src/rec/commands.js
@@ -1,6 +1,6 @@
 var commands = {};
 
-function addCommand(command, func, desc, args, flags){
+function addCommand(command, func, desc, args = null, flags = null){
     commands[command] = {
         function: func,
         desc: desc,
@@ -12,7 +12,7 @@ function addCommand(command, func, desc, args, flags){
 var hi = function(){
     return "<p><b>Test</b> <i>Test</i></p>";
 };
-addCommand("hi", hi, "Simple Test Command", null, null);
+addCommand("hi", hi, "Simple test command");
 
 var pathadd = function(args){
     if(args.length != 2){
@@ -22,7 +22,7 @@ var pathadd = function(args){
         return "";
     }
 }
-addCommand("pathadd", pathadd, "Adds argument to path", ["Path to be added"], null);
+addCommand("pathadd", pathadd, "Adds argument to path", ["Path to be added"]);
 
 var ls = function(args){
     var output = "";
@@ -38,4 +38,22 @@ addCommand("ls", ls, "Lists contents of current directory", null, ['l']);
 var ping = function(){
     return "<p><a href='http://www.google.com/'>Pong!</a></p>"
 }
-addCommand("ping", ping, "Test command for links", null, null);
+addCommand("ping", ping, "Test command for links");
+
+var help = function(){
+    var output = "<br />";
+    for(var command in commands){
+        output += "<p><b><u>" + command + "</u></b>:</p>";
+        output += "<p>&nbsp;<i>Description:</i> " + commands[command].desc + "</p>";
+        if(commands[command].args){
+            output += "<p>&nbsp;<i>Arguments:</i> " + commands[command].args + "</p>";
+        }
+        if(commands[command].flags){
+            output += "<p>&nbsp;<i>Flags:</i> " + commands[command].flags + "</p>";
+        }
+        output += "<br />";
+    }
+
+    return output;
+}
+addCommand("help", help, "Lists commands and their descriptions, arguments, and flags");
